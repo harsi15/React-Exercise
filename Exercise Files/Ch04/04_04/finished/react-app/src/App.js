8583b768626571f149ce83d59c38fc06fd1b048f
Simmy <dhillonharsimran192@gmail.com>
2024-05-23 16:09:59 +0530

    Implementation of useEffect

--- Exercise Files/Ch04/04_04/finished/react-app/src/App.js
+++ Exercise Files/Ch04/04_04/finished/react-app/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  //currentEmotion is the state variable and setEmotion is the function we use to set this state variable.
   const [currentEmotion, setEmotion] = useState("Happy");
 
+  useEffect(()=>{
+    console.log("Its ${currentEmotion} right now");
+  },[currentEmotion]);
+  
   return (
     <div className="App">
       <h1>Current emotion is {currentEmotion}</h1>
--- Exercise Files/Ch04/04_05/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_05/start/react-app/src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondaryEmotion, setSecondary] = useState("tired");
+
+  const [checkValue, setCheckValue] = ("false");
+
+  //useEffect has 2 parameters first is the event and 2 is the dependency when it should happen.
+  useEffect(()=>{
+    console.log(`Its ${emotion} emotion right`);
+  },[emotion])
+
+  //If we pass just empty [] as 2nd parameter it will print only once, i.e the event will happen only once.
+  useEffect(()=>{
+    console.log(`Its ${secondaryEmotion} around here`);
+  },[secondaryEmotion])
 
   return (
     <div className="App">
@@ -15,6 +28,14 @@ function App() {
       >
         Excited
       </button>
+
+      <h2>Secondary Emotion is {secondaryEmotion}</h2>
+      <button onClick={()=>setSecondary("grateful")}>grateful</button>
+      <button onClick={()=>setSecondary("hopeful")}>hopeful</button>
+      <br></br>
+      <h2>Now Using checkbox to test functionality</h2>
+      <input type="checkbox" value={checkValue} onChange={()=>setCheckValue((checkValue)=>!checkValue)}/>
+      <label>{checkValue ? "Checked" : "Unchecked"}</label>
     </div>
   );
 }

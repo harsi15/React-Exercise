e48b104f87aaeefb14c57919328ea52b3a0601df
Simmy <dhillonharsimran192@gmail.com>
2024-05-24 12:34:42 +0530

    fetching data from api and accessing it using component

--- Exercise Files/Ch06/06_01/start/react-app/src/App.js
+++ Exercise Files/Ch06/06_01/start/react-app/src/App.js
@@ -1,40 +1,31 @@
 import "./App.css";
-import { useState } from "react";
+import {useState, useEffect} from "react";
 
-function useInput(initialValue) {
-  const [value, setValue] =
-    useState(initialValue);
-  return [
-    {
-      value,
-      onChange: (e) => setValue(e.target.value)
-    },
-    () => setValue(initialValue)
-  ];
+function GithubUser({name,location}){
+  
+  return(
+    <div>
+      <h1>GitHub User is : {name}</h1>
+      <p> Location is <b>{location}</b></p>
+    </div>
+  )
 }
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] =
-    useState("#000000");
-  const submit = (e) => {
-    e.preventDefault();
-    alert(
-      `${titleProps.value}, ${colorProps.value}`
-    );
-    resetTitle();
-    resetColor();
-  };
+  const [data, setData] = useState(null);
+  useEffect(()=>fetch(`https://api.github.com/users/moonhighway`).then((response)=>response.json()).then(setData));
+  if (data)
+    return(
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <GithubUser name = {data.name} location = {data.location}/>
+      </div>
+      )
+ 
   return (
-    <form onSubmit={submit}>
-      <input
-        {...titleProps}
-        type="text"
-        placeholder="color title..."
-      />
-      <input {...colorProps} type="color" />
-      <button>ADD</button>
-    </form>
+    <div>
+      <h1>Data</h1>
+    </div>
   );
 }
 

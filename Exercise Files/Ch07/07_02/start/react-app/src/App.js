147a0dedbe14fc12f6361fd31313977763973a39
Simmy <dhillonharsimran192@gmail.com>
2024-05-24 18:29:23 +0530

    final updations

--- Exercise Files/Ch07/07_02/start/react-app/src/App.js
+++ Exercise Files/Ch07/07_02/start/react-app/src/App.js
@@ -1,24 +1,31 @@
 import "./App.css";
+import {Link} from "react-router-dom";
 
-function Home() {
+export function Home() {
   return (
     <div>
+      <Link to="/contact"> Contact</Link>
+      <Link to="/about">About Us</Link>
       <h1>My Website</h1>
     </div>
   );
 }
 
-function About() {
+export function About() {
   return (
     <div>
+      <Link to="/">Home</Link>
+      <Link to="/contact"> Contact</Link>
       <h1>About Us</h1>
     </div>
   );
 }
 
-function Contact() {
+export function Contact() {
   return (
     <div>
+      <Link to="/">Home</Link>
+      <Link to="/about"> About Us</Link>
       <h1>Contact Us</h1>
     </div>
   );
